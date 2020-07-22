b772343239b585f06645024426a37026882ffd19
Nazar <67974209+NazarChopko@users.noreply.github.com>
2020-07-22 15:23:31 +0300

    changes-3

--- homework-2/js/main.js
+++ homework-2/js/main.js
@@ -1,35 +1,35 @@
 
 const thisResult = document.querySelector('.result');
 //1
-let writeFirstNumber = +prompt('Please,write a number');
+let writeFirstNumber = Number(prompt('Please,write a number'));
 let writeSecondNumber
 //2
-while(isNaN(writeFirstNumber) || !Number.isInteger(writeFirstNumber) || writeFirstNumber==""){
-    writeFirstNumber = +prompt('Please,write integer number');
+while(isNaN(writeFirstNumber) || !Number.isInteger(writeFirstNumber) || writeFirstNumber===""){
+    writeFirstNumber = Number(prompt('Please,write integer number'));
 }
 
 //3
-writeSecondNumber = +prompt('Please,write second number(more than first number)');
+writeSecondNumber = Number(prompt('Please,write second number(more than first number)'));
 
 //4
-while(isNaN(writeSecondNumber) || !Number.isInteger(writeSecondNumber) || writeSecondNumber=="" || writeFirstNumber>=writeSecondNumber){
-    writeSecondNumber = +prompt('Please,write integer number too (more than first number)');
+while(isNaN(writeSecondNumber) || !Number.isInteger(writeSecondNumber) || writeSecondNumber==="" || writeFirstNumber>=writeSecondNumber){
+    writeSecondNumber = Number(prompt('Please,write integer number too (more than first number)'));
 }
 
 //5
-const evenNumbers= confirm('Do you need to skip even numbers?');
-let sumOfNumbers=0;
+const evenNumbers = confirm('Do you need to skip even numbers?');
+let sumOfNumbers = 0;
 
 //6
-if(evenNumbers) {
-    for (let j=writeFirstNumber; j<=writeSecondNumber; j++){
-        if(j % 2 !== 0){ //Шлях номер 2- if(j%2 == 0)continue
+ if(evenNumbers) {
+    for (let j = writeFirstNumber; j <= writeSecondNumber; j++){
+        if(evenNumbers && j % 2 !== 0){continue;
+        }
         sumOfNumbers += j;
     }
-    }
-}else{
-    for(let j = writeFirstNumber;j<=writeSecondNumber;j++)
-        sumOfNumbers +=j;
+    }else{
+    for(let j = writeFirstNumber;j <= writeSecondNumber;j++)
+        sumOfNumbers += j;
     
 }
 
