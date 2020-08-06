d7210786a74ac5cf6c9d2c23496a850e73046989
Nazar <67974209+NazarChopko@users.noreply.github.com>
2020-08-06 20:59:07 +0300

    homework7-1

--- homework-7/js/main.js
+++ homework-7/js/main.js
@@ -2,12 +2,21 @@ const ukraine = { tax: 0.195, middleSalary: 1789, vacancies: 11476 };
 const latvia = { tax: 0.25, middleSalary: 1586, vacancies: 3921 };
 const litva = { tax: 0.15, middleSalary: 1509, vacancies: 1114 };
 
+const task1Html = document.querySelector('.task1')
+const task2_1Html = document.querySelector('.task2')
+const task2_2Html = document.querySelector('.task3')
+const task2_3Html = document.querySelector('.task4')
+const task3_1Html = document.querySelector('.task5')
+const task3_2Html = document.querySelector('.task6')
+const task3_3Html = document.querySelector('.task7')
+
 //1
 function getMyTaxes(salary){
  return this.tax * salary;
 }
 
-console.log(getMyTaxes.call(ukraine,8000).toFixed(1))
+console.log(Number(getMyTaxes.call(ukraine,8000).toFixed(1)))
+task1Html.textContent = getMyTaxes.call(ukraine,8000).toFixed(1)
 
 
 //2
@@ -18,18 +27,28 @@ function getMiddleTaxes (){
     }
 
 
-console.log(getMiddleTaxes.call(ukraine).toFixed(1))
-console.log(getMiddleTaxes.call(latvia).toFixed(1))
-console.log(getMiddleTaxes.call(litva).toFixed(1))
+console.log(Number(getMiddleTaxes.call(ukraine).toFixed(1)))
+console.log(Number(getMiddleTaxes.call(latvia).toFixed(1)))
+console.log(Number(getMiddleTaxes.call(litva).toFixed(1)))
+
+task2_1Html.textContent = getMiddleTaxes.call(ukraine).toFixed(1)
+task2_2Html.textContent = getMiddleTaxes.call(latvia).toFixed(1)
+task2_3Html.textContent = getMiddleTaxes.call(litva).toFixed(1)
+
+
 
 //3
 function getTotalTaxes(){
     return  this.vacancies * this.middleSalary * this.tax
 }
 
-console.log(getTotalTaxes.call(ukraine).toFixed(2))
-console.log(getTotalTaxes.call(latvia).toFixed(2))
-console.log(getTotalTaxes.call(litva).toFixed(2))
+console.log(Number(getTotalTaxes.call(ukraine).toFixed(2)))
+console.log(Number(getTotalTaxes.call(latvia).toFixed(2)))
+console.log(Number(getTotalTaxes.call(litva).toFixed(2)))
+
+task3_1Html.textContent = getTotalTaxes.call(ukraine).toFixed(2)
+task3_2Html.textContent = getTotalTaxes.call(latvia).toFixed(2)
+task3_3Html.textContent = getTotalTaxes.call(litva).toFixed(2)
 
 //4
 function getMySalary(country){
