373ec02594642adc53a1956575b70f29404ff12a
Nazar <67974209+NazarChopko@users.noreply.github.com>
2020-08-21 20:55:28 +0300

    homework-11

---
+++ homework-11/js/main.js
@@ -0,0 +1,27 @@
+const button = document.querySelector(".btn");
+const answer = document.querySelector(".answer");
+
+const time = 50;
+function getRandomChinese(length) {
+  return new Promise((resolve, reject) => {
+    let current = 0;
+    let sign = parseInt(Date.now().toString().slice(-5));
+    let result = [];
+
+    while (current < length) {
+      sign += current;
+      result.push(String.fromCharCode(sign));
+      current++;
+    }
+
+    setTimeout(() => {
+      resolve(result.join(""));
+    }, time * length);
+  });
+}
+
+button.addEventListener("click", () => {
+  getRandomChinese(4).then((res) => {
+    answer.textContent = res;
+  });
+});
