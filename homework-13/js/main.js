c270b6021460cc2554e31ebbef189a51f0672da2
Nazar <67974209+NazarChopko@users.noreply.github.com>
2020-08-28 18:52:16 +0300

    homework-13

--- homework-13/js/main.js
+++ homework-13/js/main.js
@@ -1,5 +1,8 @@
 const startGenerate = document.querySelector(".res");
 const startGenerateBtn = document.querySelector(".btn");
+const plusBtn = document.getElementById("plus_btn");
+const minusBtn = document.getElementById("minus_btn");
+const res = document.getElementById("result_advance");
 
 ///TASK 1
 function* createIdGenerator() {
@@ -16,3 +19,35 @@ startGenerateBtn.addEventListener("click", () => {
 });
 
 ///TASK 2
+
+let font = 14;
+let agree;
+
+function* plusSize() {
+  while (agree === true) {
+    yield (font += 2);
+  }
+}
+
+function* minusSize() {
+  while (agree === false) {
+    yield (font -= 2);
+  }
+}
+
+let plus = plusSize();
+let minus = minusSize();
+
+plusBtn.addEventListener("click", () => {
+  agree = true;
+  res.style.fontSize = `${plus.next().value}px`;
+});
+
+minusBtn.addEventListener("click", () => {
+  agree = false;
+  res.style.fontSize = `${minus.next().value}px`;
+  if (font < 2) {
+    res.style.fontSize = `${2}px`;
+    font = 2;
+  }
+});
